0fd10f5c831b983fc9ae18a1bb66e8b65a655959
dnmyers <daniel.neil.myers@gmail.com>
2024-01-07 19:17:09 -0600

    Adding navbar and refactoring login form

--- jwtregistration.client/src/App.jsx
+++ jwtregistration.client/src/App.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+// Components
+import NavBar from "./components/NavBar/NavBar";
 
 // pages
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/login' element={<LoginPage />} />
-            </Routes>
-        </BrowserRouter>
+        <div className='app'>
+            <NavBar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
