347922ac9b54f7276676c4d7dc630c673ac87a97
dnmyers <daniel.neil.myers@gmail.com>
2024-01-07 20:02:33 -0600

    Refactor code for improved performance and readability

--- jwtregistration.client/src/App.jsx
+++ jwtregistration.client/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// components
+import NavBar from "./components/NavBar/NavBar";
+
 import "./App.css";
 
 // pages
@@ -8,11 +11,14 @@ import LoginPage from "./pages/Login";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/login' element={<LoginPage />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <NavBar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
